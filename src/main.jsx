436ffa4bcd878b26f54d259fcf8eba3897a147f8
Gautam <121284212+Gothxam@users.noreply.github.com>
2025-09-30 12:16:52 +0530

    deploye changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js';
-import { HashRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store} >
-  <HashRouter>
+
     <App />
-  </HashRouter>
   <ToastContainer />
   </Provider>
 
